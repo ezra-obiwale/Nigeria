64454e1cf7c16f3b39afa8112b7171d182a6f0a5
Ezra Obiwale <contact@ezraobiwale.com>
2017-04-04 14:03:54 +0100

    Updated the data being returned to represent just the required info

--- app/State.php
+++ app/State.php
@@ -6,5 +6,29 @@
  * @author Ezra Obiwale <contact@ezraobiwale.com>
  */
 class State extends JsonData {
-    
+
+    public static function get($node, $path = null, $limit = null, $start = 0) {
+        $resp = parent::get($node, $path, $limit, $start);
+        // fetching all states
+        if (!$path) {
+            // show only the number of lgas available
+            $resp = array_map(function($value) {
+                $value['lgas'] = count($value['lgas']);
+                return $value;
+            }, $resp);
+        }
+        // showing something specifig
+        else {
+            // remove last /
+            if ($path[strlen($path) - 1] === '/')
+                $path = substr($path, 0, strlen($path) - 1);
+            // target a state
+            if (!strstr($path, '/')) {
+                // also show only the number of lgas the state has
+                $resp['lgas'] = count($resp['lgas']);
+            }
+        }
+        return $resp;
+    }
+
 }
